cb3fbcbf0cb671fa9f1520089cb3497fa036a0a6
jame <Jame.wu@ccegroup.cn>
2018-01-08 11:39:39 +0800

    初步完成前端登录逻辑

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    var that = this;
+    wx.checkSession({
+      success: function () {
+        wx.getStorage({
+          key: 'session',
+          success: function (res) {
+            that.globalData.session = res.data;
+          },
+          fail: function () {
+            that.login();
+          }
+        });
+        wx.getStorage({
+          key: 'userInfo',
+          success: function (res) {
+            that.globalData.userInfo = res.data;
+          },
+          fail: function () {
+            that.login();
+          }
+        });
+      },
+      fail: function () {
+        that.login();
       }
     })
-    // 获取用户信息
+
+  },
+  onShow: function () {
+
+  },
+  onUnload: function () {
+  },
+  login: function () {
+    var that = this;
+    wx.login({
+      success: res => {
+        wx.getUserInfo({
+          withCredentials: true,
+          success: function (data) {
+            console.log("code:" + res.code);
+            console.log("data:" + data);
+            that.setUserInfo(data.userInfo);
+            // that.setToken(data.session);
+          },
+          fail: function (res) {
+            that.authorizeLoginFail();
+          }
+        })
+      }
+    })
+  },
+
+  authorizeLoginFail: function () {
+    var that = this;
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        } else {
+          wx.showModal({
+            title: '用户未授权',
+            content: '如需正常使用小程序，请点击授权按钮，勾选用户信息。',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                wx.openSetting({
+                  success: (res) => {
+                    that.login();
+                  }
+                })
               }
             }
           })
@@ -33,7 +78,22 @@ App({
       }
     })
   },
+  setUserInfo: function (data) {
+    this.globalData.userInfo = data;
+    wx.setStorage({
+      key: "userInfo",
+      data: data
+    })
+  },
+  setSession: function (data) {
+    this.globalData.session = data;
+    wx.setStorage({
+      key: "session",
+      data: data
+    })
+  },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    session: null
   }
 })
