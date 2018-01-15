38fb46de2cf82900adb7393d606de9d00bb58a36
jame <Jame.wu@ccegroup.cn>
2018-01-15 17:53:37 +0800

    为了更新 看看波波代码

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,7 +1,16 @@
 Page({
-  data: { tag: '', location: '' },
+  data: { tag: '', location: ''},
 
   onLoad: function (options) {
     this.setData({ tag: options.tag || '', location: options.location || '' })
+  },
+  closeInput: function (){
+    console.log("close");
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  reloadInput: function (){
+      console.log("reload");
   }
 })
--- pages/search/index.js
+++ pages/search/index.js
@@ -3,10 +3,28 @@ const app = getApp();
 Page({
   data: {
     tags: ['吃饭', '旅游', '食物', '风景', 'uiux', '食物'],
-    locations: ['北京', '上海', '三亚', '亚丁']
+    locations: ['北京', '上海', '三亚', '亚丁','河南'],
+    showMainWords:true, // 显示标签，地点的关键搜索词开关
+    inputValue:'',  // 输入框 默认值
+    suggestList:['上海','上火','上山']  // 模拟数据，猜词列表
   },
 
   onLoad: function() {
 
+  },
+  /**
+   * input 获取焦点
+   */
+  inputFocus: function() {
+   
+  },
+  /**
+   * input 获取输入内容
+   */
+  inputTyping: function(e) {
+    this.setData({
+      showMainWords: false
+    })
+    console.log("正在输入:"+e.detail.value);
   }
 })
